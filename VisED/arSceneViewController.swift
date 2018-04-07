303f68d36686c000db59d3b515a671786e1c1c6a
Mohak Malhotra <mohakmalhotra@rocketmail.com>
2018-04-07 18:49:41 +1000

    got basic ar stuff working

--- VisED/arSceneViewController.swift
+++ VisED/arSceneViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import ARKit
 import Firebase
-class arSceneViewController: UIViewController {
+class arSceneViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var arView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
@@ -17,6 +17,16 @@ class arSceneViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        arView.delegate = self
+        
+        }
+     
+    
+
+    
+       
+    
+    override func viewDidAppear(_ animated: Bool) {
         let storage = Storage.storage()
         let modelRef = storage.reference(forURL: "gs://vised-8c24e.appspot.com/t1/wk1/y8bi001mdl1/dna.scn")
         let nsDocumentDirectory:FileManager.SearchPathDirectory = FileManager.SearchPathDirectory.documentDirectory
@@ -24,16 +34,15 @@ class arSceneViewController: UIViewController {
         let nsUserDomainMask:FileManager.SearchPathDomainMask = FileManager.SearchPathDomainMask.userDomainMask;
         
         let paths:[String] = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory, nsUserDomainMask, true);
-         var folderPath = ""
+        var folderPath = ""
         if (paths.count > 0) {
             
-          folderPath = String(paths[0]);
+            folderPath = String(paths[0]);
             folderPath =  "file:" + folderPath + "/dna.scn"
             print(folderPath);
-           
+        
         }
         let  localUrl = URL(string: folderPath)!
-    
         let downloadTask = modelRef.write(toFile: localUrl) { url, error in
             if let error = error {
                 print (error)
@@ -41,25 +50,28 @@ class arSceneViewController: UIViewController {
                 // Local file URL for "images/island.jpg" is returned
                 let finalUrl = url!
                 print(url)
-                self.arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-                self.arView.session.run(self.configuration)
+                    //-----end firebase storage
+            
                 do{
-                
-                let scene = try SCNScene(url:  finalUrl, options: nil)
                     
-            } catch {}
-              
+                    let scene =  SCNScene()
+                     self.arView.scene = scene
+                    self.arView.session.run(self.configuration)
+                    var nodeModel:SCNNode!
+                    let modelScene = try SCNScene(url:  finalUrl, options: nil)
+                    let nodeName = "face0"
+                    nodeModel = modelScene.rootNode.childNode(
+                        withName: nodeName, recursively: true)
+                    nodeModel.position = SCNVector3Zero
+                    self.arView.scene.rootNode.addChildNode(nodeModel)
+                    
+                } catch {}
+                
                 
             }
-        }
-     
-    
-
-        
-       
-       
-        // Do any additional setup after loading the view.
     }
+    }
+        
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
